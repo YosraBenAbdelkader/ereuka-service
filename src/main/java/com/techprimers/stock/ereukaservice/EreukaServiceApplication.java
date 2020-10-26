047f59438c76989681f89021d049660de81274a4
Yosra <yosra.benabdelkader@gmail.com>
2020-10-26 16:28:30 +0100

    first commit : ereuka-service

---
+++ src/main/java/com/techprimers/stock/ereukaservice/EreukaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.techprimers.stock.ereukaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EreukaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EreukaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techprimers/stock/ereukaservice/EreukaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.stock.ereukaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EreukaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
