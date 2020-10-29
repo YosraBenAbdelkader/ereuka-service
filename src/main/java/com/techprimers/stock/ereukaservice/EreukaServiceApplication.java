c3c6fc48c18e7b73b09e4ac4c86c122be40ba3b2
Yosra <yosra.benabdelkader@gmail.com>
2020-10-29 02:22:21 +0100

    commit : add zuul config

--- src/main/java/com/techprimers/stock/ereukaservice/EreukaServiceApplication.java
+++ src/main/java/com/techprimers/stock/ereukaservice/EreukaServiceApplication.java
@@ -3,7 +3,10 @@ package com.techprimers.stock.ereukaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class EreukaServiceApplication {
